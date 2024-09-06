bad79a11ec81ab74c3642ef242f1204b90a50c7e
Efocor <felipe.correa@pregrado.uoh.cl>
2024-09-05 21:54:01 -0400

    Cambio pequeño de prueba

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <li>Noticias</li>
         <li>Tools</li>
         <li><button className='btn'>Contacto</button></li>
-        <li>Placeholder para buscador</li>
+        <li>Placeholder para search</li>
       </ul>
     </nav>
   )
