ace7f2a0988f67260a7b2649f41dc046ea1bbf34
Efocor <felipe.correa@pregrado.uoh.cl>
2024-11-12 21:28:30 -0300

    arreglo numero 5000650454

--- src/pages/miembro2.tsx
+++ src/pages/miembro2.tsx
@@ -5,7 +5,7 @@ import {FC, memo} from 'react';
 import Page from '../components/Layout/Page';
 import Footer from '../components/Sections/Footer';
 import backgroundImage from '../images/header-background.webp'; // Imagen de fondo
-import alexImage from '../images/Perfil_Alex_Genova.jpg';
+import alexImage from '../images/perfil_alex_genova.jpg';
 
 // Importación dinámica del Header
 const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
