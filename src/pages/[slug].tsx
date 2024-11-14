17c7337aa63a70a62ba95167deb58f301ed20dfe
Efocor <felipe.correa@pregrado.uoh.cl>
2024-11-14 20:51:39 -0300

    Fix numro 50y646943

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -27,7 +27,7 @@ const CMSPage: FC<{ post: any }> = memo(({ post }) => {
         <div className="z-10 max-w-4xl w-full mt-20 bg-white bg-opacity-90 backdrop-blur-md shadow-2xl rounded-xl p-8 flex flex-col items-center transition-shadow duration-300 hover:shadow-blue-500/50">
           <button
             className="absolute top-4 right-4 px-3 py-1 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-300"
-            onClick={() => window.history.back()}>
+            onClick={() => window.history.back()} >
             Volver
           </button>
           <h3 className="text-4xl font-bold text-blue-700 mb-2 tracking-wide hover:underline hover:text-blue-500 transition duration-300">
@@ -59,9 +59,15 @@ export async function getStaticPaths() {
   const postsDirectory = path.join(process.cwd(), 'src/pages/blog');
   const filenames = fs.readdirSync(postsDirectory);
 
-  const paths = filenames.map((filename) => ({
-    params: { slug: filename.replace(/\.md$/, '') },
-  }));
+  const paths = filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const { data } = matter(fileContent);
+
+    return {
+      params: { slug: data.slug || filename.replace(/\.md$/, '') }, // Usa el slug del frontmatter o el nombre del archivo
+    };
+  });
 
   return {
     paths,
@@ -75,7 +81,7 @@ export async function getStaticProps({ params }: { params: { slug: string } }) {
   const fileContent = fs.readFileSync(filePath, 'utf-8');
   const { data, content } = matter(fileContent);
 
-  // Asegúrate de convertir la fecha a una cadena
+  // Asegúrate de convertir la fecha a una cadena si es necesario
   const postDate = new Date(data.date).toISOString(); // O el formato que prefieras
 
   return {
